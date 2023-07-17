b275b166d48772a7340639eed8e535004e08eda5
Nadya Gallyamova <lomeshyza@yandex.ru>
2023-07-18 00:52:51 +0300

    feature: change port to 3001

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use(errors());
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   // eslint-disable-next-line no-console
-  console.log('App listening on port 3000');
+  console.log('App listening on port 3001');
 });
